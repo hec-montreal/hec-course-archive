2e55692df7fa4d0d133987fc163bc6b477ff15a3
Curtis van Osch <curtis.van-osch@hec.ca>
2012-12-13 18:05:22 +0000

    Première initialisation de l'outil d'archives

---
+++ api/src/main/java/ca/hec/archive/logic/ProjectLogic.java
@@ -0,0 +1,20 @@
+package ca.hec.archive.logic;
+
+import java.util.List;
+
+import ca.hec.archive.model.Item;
+
+/**
+ * An example logic interface
+ * 
+ * @author Mike Jennings (mike_jennings@unc.edu)
+ *
+ */
+public interface ProjectLogic {
+
+	/**
+	 * Get a list of Items
+	 * @return
+	 */
+	public List<Item> getItems();
+}
